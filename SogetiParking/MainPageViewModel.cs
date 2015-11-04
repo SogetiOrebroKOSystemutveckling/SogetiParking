0592cf4f30fa0376dc4b545b6cee6555bf936258
Vimefall <anders.vimefall@gmail.com>
2015-11-04 19:32:20 +0100

    Trying to retrieve sensors, something's wrong though. Fix!

--- SogetiParking/MainPageViewModel.cs
+++ SogetiParking/MainPageViewModel.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Org.Json;
 using Xamarin.Forms;
 
 namespace SogetiParking
@@ -12,12 +17,56 @@ namespace SogetiParking
 			ColdGarageRight = "Ledig";
 			WarmGarageLeft = "Upptagen";
 			WarmGarageRight = "Ledig";
+
+		    Garages = GetGarages();
 		}
 
-		public string ColdGarageLeft { get; set; }
+	    public List<Garage> Garages { get; set; } 
+
+	    public List<Garage> GetGarages()
+	    {
+	        var request = WebRequest.Create("http://localhost:8080/api/Sensor/GetSensors");
+	        request.ContentType = "application/json";
+	        request.Method = "GET";
+
+	        using (var response = request.GetResponse())
+	        using (var responseStream = response.GetResponseStream())
+	        {
+	            var reader = new StreamReader(responseStream);
+	            var content = reader.ReadToEnd();
+
+	            return JsonConvert.DeserializeObject<List<Garage>>(content);
+	        }
+	    }
+
+	    public string ColdGarageLeft { get; set; }
 		public string ColdGarageCenter { get; set; }
 		public string ColdGarageRight { get; set; }
 		public string WarmGarageLeft { get; set; }
 		public string WarmGarageRight { get; set; }
 	}
+
+    public class Garage
+    {
+        public Garage(string name)
+        {
+            Name = name;
+            ParkingLots = new List<ParkingLot>();
+        }
+
+        public string Name { get; private set; }
+
+        public List<ParkingLot> ParkingLots { get; set; }
+    }
+
+    public class ParkingLot
+    {
+        public ParkingLot(string lotNumber)
+        {
+            LotNumber = lotNumber;
+        }
+
+        public bool IsFree { get; set; }
+        public string LotNumber { get; set; }
+    }
 }
