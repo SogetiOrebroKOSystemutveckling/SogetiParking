d068ea71f42eee4bd28fecea0a2aa075182b6100
Markus Carlen <markus.carlen@sogeti.se>
2016-02-11 13:14:07 +0100

    Added db

--- SensorServiceAzure/Global.asax.cs
+++ SensorServiceAzure/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Newtonsoft.Json.Serialization;
 
 namespace SensorServiceAzure
 {
@@ -12,6 +13,10 @@ namespace SensorServiceAzure
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
